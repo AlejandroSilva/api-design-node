b2da406043c0b0e5bc7cf0869abc66885982b073
AlejandroSilva <pm5k.sk@gmail.com>
2015-07-22 16:43:22 -0300

    task completed + fixed errors

--- server/lions.js
+++ server/lions.js
@@ -1,4 +1,5 @@
 var lionRouter = require('express').Router();
+var _ = require('lodash');
 
 var lions = [];
 var id = 0;
@@ -12,13 +13,14 @@ var updateId = function(req, res, next) {
 };
 
 lionRouter.param('id', function(req, res, next, id) {
-  var todo = _.find(todos, {id: id});
+  var lion = _.find(lions, {id: id});
 
-  if (todo) {
-    req.todo = todo;
+  if (lion) {
+    req.lion = lion;
     next();
   } else {
-    res.send();
+    //res.send('not found');
+    next(new Error('not found'));
   }
 });
 
@@ -27,7 +29,7 @@ lionRouter.get('/', function(req, res){
 });
 
 lionRouter.get('/:id', function(req, res){
-  var lion = req.todo;
+  var lion = req.lion;
   res.json(lion || {});
 });
 
